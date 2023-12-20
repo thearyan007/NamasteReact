c5e7944568dac285a284cde30a9692d1509472c9
thearyan007 <thearyangupta007@gmail.com>
2023-12-20 14:09:45 +0530

    renamed to app1

--- App1.js
+++ App1.js
@@ -1,3 +1,4 @@
+// for printing the hello world
 const heading = React.createElement("h1", {}, "Hello World!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
