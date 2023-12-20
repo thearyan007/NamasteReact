2a0ac72a3120242e9b3dc819895476330b1ad9d8
thearyan007 <thearyangupta007@gmail.com>
2023-12-20 14:26:31 +0530

    created parent and child structure without EJX

--- Lecture1/App2.js
+++ Lecture1/App2.js
@@ -1,1 +1,31 @@
-//  need to create the folowing
+//  need to create the folowing structure:
+/* 
+    <div id = "root">
+        <div id = "parent">
+            <div id = "child1">
+                <h1>H1 tag</h1>
+                <h2>H2 tag</h2>
+            </div>
+            <div id = "child2">
+                <h1>H1 tag</h1>
+                <h2>H2 tag</h2>
+            </div>
+        </div>
+    </div> 
+
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "H1 tag"),
+    React.createElement("h2", {}, "H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "H1 tag"),
+    React.createElement("h2", {}, "H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
