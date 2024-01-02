a5536415a5ba71cf6cf768763c3932712ae28acc
thearyan007 <thearyangupta007@gmail.com>
2024-01-02 15:56:05 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// for printing the hello world using react
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "Hello World!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
