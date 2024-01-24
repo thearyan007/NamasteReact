947565be7f93299868916bfa0764a90307829ff4
thearyan007 <thearyangupta007@gmail.com>
2024-01-24 13:33:55 +0530

    added Shimmer component

--- Lecture4-5/src/components/Body.js
+++ Lecture4-5/src/components/Body.js
@@ -1,9 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Restaurants from "./Restaurants";
-import restroData from "../util/restroData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  let [RestroList, setRestroList] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    const fetchedRestro =
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    console.log(fetchedRestro);
+    setRestroList(fetchedRestro);
+  };
+  if (RestroList.length === 0) {
+    return <Shimmer />;
+  }
+
   return (
     <div className="body-container">
       <div className="search">
@@ -16,8 +38,22 @@ const Body = () => {
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
+      <div className="flter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const FilteredList = RestroList.filter(
+              (restro) => restro.info.avgRating > 4
+            );
+            setRestroList(FilteredList);
+            console.log(RestroList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurants-container">
-        {restroData.map((restro) => {
+        {RestroList.map((restro) => {
           return <Restaurants key={restro.info.id} restData={restro} />;
         })}
       </div>
