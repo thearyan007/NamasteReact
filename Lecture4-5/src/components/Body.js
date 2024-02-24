07138f2e30805f600c44508eacd6ead2ce6fdd6a
thearyan007 <thearyangupta007@gmail.com>
2024-02-24 23:06:05 +0530

    revisedAPI

--- Lecture4-5/src/components/Body.js
+++ Lecture4-5/src/components/Body.js
@@ -17,7 +17,7 @@ const Body = () => {
 
   const getData = async () => {
     let data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
